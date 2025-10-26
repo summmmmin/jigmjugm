71f62cc1cd5c2096845916a253bdfacd00c6dbdd
하수민 <mini338848@gmail.com>
2025-10-27 08:00:14 +0900

    fix:Pageable 임포트

--- src/main/java/com/jigmjugm/challenge/repo/ChallengeRoundRepository.java
+++ src/main/java/com/jigmjugm/challenge/repo/ChallengeRoundRepository.java
@@ -2,7 +2,7 @@ package com.jigmjugm.challenge.repo;
 
 import com.jigmjugm.challenge.domain.ChallengeRound;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRoundRepository extends JpaRepository<ChallengeRound, Long> {
